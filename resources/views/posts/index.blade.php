31bc1276f6a16a972dde3d30a18daa59607f4d27
shogo4321 <k.shougo0921@gmail.com>
2024-04-28 18:39:49 +0900

    Add ブログ投稿作成画面作成

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,11 +3,14 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+   
     </head>
     <body>
         <h1>Blog Name</h1>
+        <a href="/posts/create">create</a>
         <div class='posts'>
             @foreach($posts as $post)
                 <div class='post'>
